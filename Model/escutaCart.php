8af44da6083abbfa1d6a0943bd505d1a5ad19df9
RobertoSRocha <roberto_rocha_11@hotmail.com>
2018-05-21 15:35:40 -0300

    Ajustando o escutaCart

--- Model/escutaCart.php
+++ Model/escutaCart.php
@@ -1,55 +1,28 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>keyLabepi</title>
-    </head>
-    <body>
-        <?php
-            include("user_interface.php");
-            include("log_interface.php");
-            
-            $idCart = null;
-            //VERIFICA A EXISTÊNCIA DA VARIÁVEL E SE HÁ CONTEÚDO NELA
-            if(isset($_GET["a"]) && !empty($_GET["a"])){
-                $idCart = $_GET["a"];
-                echo "Cartão lido: $idCart</br>";
-                $aux = verificaCart($idCart);
-                if($aux){
-                    //EXIBE LED VERDE
-                    echo "</br>Usuário válido</br></br>";
-                    guardarLog("permitido", $aux, $idCart);
-                    
-                    //return 1;
-                }
-                else{
-                    //EXIBE LED VERMELHO
-                    echo "</br>Usuário inválido</br>";
-                    guardarLog("negado", $aux, $idCart);
-                    //return 0;
-                }
-            }
-            else{
-                //EXIBE LED AZUL
-                echo "Dados insuficientes!</br>";
-                //return 0;
-            }
-
-            /*
-            //TESTES
-            cadastrarPessoa("Tafareu", "AIAI554477", 2014000000);
-            excluirUsu("AIAI554477");
-            listarPorNome("ronaldinho");
-            listarPorCartao("ABCD010203");
-            listarPorMatricula(20140602)
-            excluirLog(23);
-            excluirLogUser(2);
-            listarLogs();
-            listarLogsNeg();
-            listarLogsPerm();
-            listarLogsData('2018-05-05');
-            listarLogsData('2018-05-05');
-             * */
-        ?>
-    </body>
-</html>
+<?php
+    include("user_interface.php");
+    include("log_interface.php");
+    $idCart = null;
+    //VERIFICA A EXISTÊNCIA DA VARIÁVEL E SE HÁ CONTEÚDO NELA
+    if(isset($_GET["a"]) && !empty($_GET["a"])){
+        $idCart = $_GET["a"];
+        echo "Cartão lido: $idCart</br>";
+        $aux = verificaCart($idCart);
+        if($aux){
+            //EXIBE LED VERDE
+            echo "</br>Usuário válido</br></br>";
+            guardarLog("permitido", $aux, $idCart);
+            //return $idCart.'|'.1;
+        }
+        else{
+            //EXIBE LED VERMELHO
+            echo "</br>Usuário inválido</br>";
+            guardarLog("negado", $aux, $idCart);
+            //return $idCart.'|'.0;
+        }
+    }
+    else{
+        //EXIBE LED AZUL
+        echo "Dados insuficientes!</br>";
+        //return $idCart.'|'.0;
+    }
+   
